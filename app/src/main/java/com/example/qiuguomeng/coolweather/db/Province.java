4cc03d9c325a87604140357b490107c03266ed68
Gmeng <qiuguomeng@163.com>
2017-01-22 21:55:09 +0800

    add database class

---
+++ app/src/main/java/com/example/qiuguomeng/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.example.qiuguomeng.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by qiuguomeng on 2017/1/22.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
